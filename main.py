d7e5f265bfd8b9c038cf10d899071f43d5702fdc
anandvegaraju <v.anand.vamsi@gmail.com>
2017-04-26 08:37:36 +0530

    small update

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import json
+
 from pprint import pprint
 
 with open('vibez.json') as data_file:
     data = json.load(data_file)
 
 node = input()
+list = data[node].split()
 
 pprint(data[node])
+
+print("\n")
+
+
+for i in list:
+    data1 = data[i]
+    pprint(data1)
+    print("\n")
+    print("-----------------------------------------------------------------------")
+
 
